68989f23971a4e133a8e3eff5778283894024d68
aurellieandra <diandra.azra@gmail.com>
2022-07-11 21:23:21 +0700

    fixing middleware cloudinary .env

--- app/middleware/cloudinary.js
+++ app/middleware/cloudinary.js
@@ -1,9 +1,15 @@
 const cloudinary = require('cloudinary').v2
 
+const {
+    cloud_name,
+    api_key,
+    api_secret
+} = process.env;
+
 cloudinary.config({
-    cloud_name: "aurellieandra",
-    api_key: "986524165148345",
-    api_secret: "R8XJmHFXfllq_x-iKlibzFd3R48"
+    cloud_name,
+    api_key,
+    api_secret
 })
 
 module.exports = cloudinary;
