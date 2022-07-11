10ba6f60a1251f188ada1c475076f0dee311d5d4
aurellieandra <diandra.azra@gmail.com>
2022-07-12 00:29:28 +0700

    changing multer path trial and adding images

--- app/middleware/multer.js
+++ app/middleware/multer.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "../../public/uploads")
+        cb(null, "../public/uploads")
     },
     filefilter: function (req, file, cb) {
         let ext = path.extname(file.originalname)
