3c177505835a63d72c35ab4d1131dcf7e0179368
aurellieandra <diandra.azra@gmail.com>
2022-07-12 13:04:53 +0700

    changing path multer

--- app/middleware/multer.js
+++ app/middleware/multer.js
@@ -2,9 +2,6 @@ const multer = require('multer')
 const path = require('path')
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "../public/uploads")
-    },
     filefilter: function (req, file, cb) {
         let ext = path.extname(file.originalname)
         if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
