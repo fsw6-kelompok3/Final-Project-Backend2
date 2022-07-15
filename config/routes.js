0689f284afa8d3ce32051bf5ffc6cce1a6beab98
aurellieandra <diandra.azra@gmail.com>
2022-07-15 22:18:19 +0700

    adding more cors

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const controllers = require("../app/controllers");
 
+const cors = require("cors")
+
 const multer = require('../app/middleware/multer')
 const authUser = require('../app/middleware/user')
 const authAdmin = require('../app/middleware/admin')
@@ -8,6 +10,7 @@ const authAdmin = require('../app/middleware/admin')
 const appRouter = express.Router();
 const apiRouter = express.Router();
 
+appRouter.use(cors())
 /** Mount GET / handler */
 appRouter.get("/", controllers.main.index);
 
