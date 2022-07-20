b443310ea51c63afcefc4d5ed31544946c7bd950
aurellieandra <diandra.azra@gmail.com>
2022-07-20 23:33:47 +0700

    content type fix

--- app/index.js
+++ app/index.js
@@ -33,7 +33,7 @@ app.use(cors({
     credentials: true,
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    headers: '*'
+    headers: 'Content-Type'
 }))
 
 /** Install View Engine */
