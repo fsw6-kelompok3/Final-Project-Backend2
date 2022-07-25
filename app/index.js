d25136c40aab303c00ff980b2972de1aa6aeec7c
aurellieandra <diandra.azra@gmail.com>
2022-07-25 12:20:07 +0700

    finalizing to origin vercel

--- app/index.js
+++ app/index.js
@@ -14,7 +14,7 @@ const viewsDir = path.join(__dirname, "./views");
 const app = express();
 
 var allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', "http://localhost:3000");
+    res.header('Access-Control-Allow-Origin', "https://secondhandbook.vercel.app");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
     next();
 };
@@ -30,7 +30,7 @@ app.use(express.json());
 /** Instal Cors */
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:3000',
+    origin: 'https://secondhandbook.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
 }))
 
